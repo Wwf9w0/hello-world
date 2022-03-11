7ac4e4acac8dff80662382d5c525f279fd692713
wwf9w0 <aliemrekaradag1@gmail.com>
2022-03-11 05:58:21 +0300

    first commit

---
+++ first.go
@@ -0,0 +1,14 @@
+package main
+
+import "net/http"
+import "github.com/gorilla/mux"
+
+func yourHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Gorilla//"))
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", yourHandler)
+	panic(http.ListenAndServe(":8080", r))
+}
